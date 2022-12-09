f25d687cb427752311dff1e9672b327b2f1850ba
Joaquin Barrera <joaquincbarrera@gmail.com>
2022-12-09 11:21:09 -0300

    final curso platzi react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client"; //version 18
+//import ReactDOM from 'react-dom'; //versiones anteriores a 18
+import "./index.css";
+import App from "./App/index.js";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//version 18
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//versiones anteriores a 18
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// );
